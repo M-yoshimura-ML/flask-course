f45ec19c02d2c55b28310a72f31a928a1f3a2ced
M-yoshimura-ML <masadrst@gmail.com>
2024-06-02 19:11:11 +0800

    Flask hello world

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from flask import Flask
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
 def hello_world():
-    print("Hello PyCharm")
+    return "Hello world"
 
 
 if __name__ == "__main__":
-    hello_world()
+    app.run()
