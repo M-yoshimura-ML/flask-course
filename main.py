81587e965ddddd45e648aa6f8beecc4875f905b8
M-yoshimura-ML <masadrst@gmail.com>
2024-06-07 12:36:06 +0800

    install pymysql & change db config

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "YOUR_SECRET_KEY"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///flask_blog.db"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///flask_blog.db"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost:port/db_name'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password!123@localhost:3306/flask_blog'
     csrf.init_app(app)
     db.init_app(app)
     with app.app_context():
