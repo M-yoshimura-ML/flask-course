9988cb3b5de083972ef2946b8189b3e9883e1807
M-yoshimura-ML <masadrst@gmail.com>
2024-06-09 09:36:47 +0800

    Displaying role in user list & adding admin user

--- insert_data.py
+++ insert_data.py
@@ -1,5 +1,5 @@
 from main import db, create_app
-from models.user import Role
+from models.user import Role, User
 
 
 app = create_app()
@@ -12,4 +12,8 @@ with app.app_context():
     if not Role.query.filter_by(name='user').first():
         user_role = Role(name='user')
         db.session.add(user_role)
+
+    admin = User.query.filter_by(email='admin@gmail.com').first()
+    if admin:
+        admin.role_id = 1
     db.session.commit()
