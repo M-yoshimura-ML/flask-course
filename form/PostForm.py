2a21313041648b9a93ea882bef46c984c697bd71
M-yoshimura-ML <masadrst@gmail.com>
2024-06-11 12:17:32 +0800

    custom validation: unique check for slug

--- form/PostForm.py
+++ form/PostForm.py
@@ -4,6 +4,8 @@ from wtforms.validators import DataRequired, Length, ValidationError
 from wtforms.widgets.core import TextArea
 import re
 
+from models.post import Post
+
 
 def validate_slug(form, field):
     slug = field.data
@@ -11,8 +13,15 @@ def validate_slug(form, field):
         raise ValidationError("Slug must contain only letters, numbers, and hyphens.")
 
 
+def validate_unique_slug(form, field):
+    slug = field.data
+    existing_post = Post.query.filter_by(slug=slug).first()
+    if existing_post:
+        raise ValidationError("This slug is already in use. Please choose different one.")
+
+
 class PostForm(FlaskForm):
     title = StringField("title", [DataRequired("Please enter title."), Length(min=3, max=100)])
     content = StringField("content", [DataRequired("Please enter content.")], widget=TextArea())
-    slug = StringField("slug", [DataRequired("Please enter slug."), validate_slug])
+    slug = StringField("slug", [DataRequired("Please enter slug."), validate_slug, validate_unique_slug])
     submit = SubmitField("submit")
