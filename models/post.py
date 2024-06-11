7e0fc0e25e001bff270e8ef3ccfc1604d2a6488f
M-yoshimura-ML <masadrst@gmail.com>
2024-06-11 11:50:20 +0800

    Adding Custom Validation with Regex for slug

--- models/post.py
+++ models/post.py
@@ -7,6 +7,6 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    slug = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.datetime.now())
